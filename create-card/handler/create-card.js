f14f1026787888900e59720ef2115ed482cb6636
NaranjaX <naranjax@MAC-67663.local>
2022-08-29 17:16:46 -0300

    test: log normalizedEvent

--- create-card/handler/create-card.js
+++ create-card/handler/create-card.js
@@ -1,8 +1,10 @@
 const { batchEventMapper } = require('ebased/handler');
 const inputMode = require('ebased/handler/input/batchEventQueue');
 const outputMode = require('ebased/handler/output/batchEventConfirmation');
-const { createGiftDomain } = require('../domain/create-gift.domain');
+const { createGiftDomain } = require('../domain/create-card.domain');
 
 module.exports.handler = async (events, context) => {
+  const normalizedEvent = (events) => events.Records.map((record) => JSON.parse(record.body));
+  console.log(normalizedEvent(events));
   return batchEventMapper({ events, context }, inputMode, createGiftDomain, outputMode);
 }
--- create-gift/handler/create-gift.js
+++ create-gift/handler/create-gift.js
@@ -4,5 +4,7 @@ const outputMode = require('ebased/handler/output/batchEventConfirmation');
 const { createGiftDomain } = require('../domain/create-gift.domain');
 
 module.exports.handler = async (events, context) => {
+  const normalizedEvent = (events) => events.Records.map((record) => JSON.parse(record.body));
+  console.log(normalizedEvent(events));
   return batchEventMapper({ events, context }, inputMode, createGiftDomain, outputMode);
 }
